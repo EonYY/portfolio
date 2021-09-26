84a0dadea59ff1a159596d2bf19579309cb8e292
EonYY <eonblues@outlook.com>
2021-09-26 15:26:32 +0300

    Insert background image

--- site/pages/index.js
+++ site/pages/index.js
@@ -1,12 +1,21 @@
+import { makeStyles } from "@mui/styles"
 
 
 
+const useStyles = makeStyles(() => ({
+  root: {
+    minHeight: '100vh',
+    backgroundImage: `url(${'/assets/Background.jpg'})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+  },
+}));
 
 
 export default function Home() {
-  return (
-    <div>
-      One Page
-    </div>
-  )
-}
+  const classes = useStyles();
+
+    return (
+      <div className={classes.root}></div>
+    )
+  }
