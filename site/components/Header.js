c043dcb56b643ad65c2b2c4c4a4ca502d22d6808
EonYY <eonblues@outlook.com>
2021-09-26 16:58:13 +0300

    Add theme color

--- site/components/Header.js
+++ site/components/Header.js
@@ -1,19 +1,39 @@
-import { AppBar } from "@mui/material";
-import { makeStyles } from "@mui/styles"
+import { IconButton, AppBar, Toolbar } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import SortIcon from "@mui/icons-material/Sort"
 
-
-
-const useStyles = makeStyles(() => ({}));
-
-
-export default function Home() {
-  const classes = useStyles();
-
-    return (
-      <div>
-        <AppBar>
-            <h1>Header</h1>
+const useStyles = makeStyles(() => ({
+    appBar: {
+        background: 'none',
+    },
+    appBarTitle: {
+        flexGrow: '1',
+    },
+    appBarWrapper: {
+        width: '80%',
+        margin: '0 auto',
+    },
+    icon: {
+        color: '#fff',
+        fontSize: '2rem',
+    },
+    textThemeColor: {
+        color: '#ff409f',
+    }
+  }));
+  
+  
+  export default function Header() {
+    const classes = useStyles();
+  
+      return (
+        <AppBar className={classes.appBar} elevation={0}>
+            <Toolbar className={classes.appBarWrapper}>
+                <h1 className={classes.appBarTitle}>My<span className={classes.textThemeColor}>Skills.</span></h1>
+                <IconButton>
+                    <SortIcon className={classes.icon}/>
+                </IconButton>
+            </Toolbar>
         </AppBar>
-      </div>
-    )
-  }
+      )
+    }
